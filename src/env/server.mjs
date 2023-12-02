f52980e4f24da06be19730dc3690d5817b9d24a5
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-02 09:05:56 +0900

    t3の元の方ではserver.tsだとruntimeEnvでやっているのでそれにしてみる

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -10,7 +10,8 @@ export const env = createEnv({
     POPULATION_API_URL: z.string().url(),
    },
    // envファイルと紐づける
-   experimental__runtimeEnv:{
+   //invalid URLと出た。
+   runtimeEnv:{
         API_KEY: "API_KEY",
         PREFECTURES_API_URL: "PREFECTURES_API_URL",
         POPULATION_API_URL: "POPULATION_API_URL",
