9bd00c532b3bb2d36cdab049f5841065633db23d
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-10 03:18:27 +0900

    Cloudflareで動かすための変更。これでできるかをテストする

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ import "./src/env/client.mjs";
 import "./src/env/server.mjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        runtime: 'experimental-edge',
+      },
+};
 
 export default nextConfig;
