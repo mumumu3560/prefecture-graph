b092840ce061eb7d28d5c59d6c380b01d165736b
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-10 02:24:35 +0900

    checkBox_serverではなくて、CookieSave.tsxを使うこととしていた。あとでcheckBox_serverは消す。 説明は同様でここでAPIからデータ取得

---
+++ src/app/components/PrefecturesField/PrefecturesField_logic.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { fetchPopulation } from "@/app/components/api_relation/fecth_population";
+
+export async function CookieGet(): Promise<PopulationResult | null> {
+
+    console.log("CookieGet is called");
+    const cookieStore = cookies();
+    
+    const fetchPrefCode = decodeURIComponent(cookieStore.get("prefCode")?.value ?? "");
+    const fetchPrefName = decodeURIComponent(cookieStore.get("prefName")?.value ?? "");
+
+    const isAfterRemove = decodeURIComponent(cookieStore.get("isAfterRemove")?.value ?? "");
+    console.log("ttttttttttt");
+    console.log("isAfterRemove:", isAfterRemove);
+
+    if(fetchPrefCode == "" || fetchPrefName == "" || isAfterRemove !== "false"){
+        console.log("cookie is empty");
+        return null;
+    }
+
+    console.log("fetchPrefCode:", fetchPrefCode);
+
+    const populationResult2: PopulationResult = await fetchPopulation(Number(fetchPrefCode), fetchPrefName);
+    
+    return populationResult2;
+}
