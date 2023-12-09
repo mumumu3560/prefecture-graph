5544fc150d253ce23004f4030139bf64f0d75316
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-10 02:38:04 +0900

    ここでPopulationResult(RESASから受け取るデータ型とほぼ同じ)からRechartsで使うデータ型への変更を行いたい

---
+++ src/app/components/api_relation/data_formatting.ts
@@ -0,0 +1,22 @@
+interface PopulationData {
+  year: number;
+  value: number;
+  rate?: number;
+}
+
+interface PopulationCategory {
+  label: string;
+  data: PopulationData[];
+}
+
+interface PopulationResult {
+  prefName: string;
+  data: PopulationCategory[];
+}
+
+interface ChartDataItem {
+  year: number;
+  [key: string]: number;
+}
+
+
