017c9939e3b5ddf414fe5347dcbb5215ca4b556b
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-10 02:36:45 +0900

    zustandを用いた状態管理関係の追加。 チェックが外される(元々isChecked=true)とzustandを使い、都道府県情報を削除し、routerのrefreshを行う(これはいらないか？) チェックが付くとcookieに都道府県の情報を入れてrouterをrefreshし、SCで情報を受け取るようにする。

--- src/app/components/PrefecturesField/CheckBox/CheckBox_view.tsx
+++ src/app/components/PrefecturesField/CheckBox/CheckBox_view.tsx
@@ -1,39 +1,93 @@
 "use client";
 
 import React, { useState } from "react";
-import { CheckBoxLogic } from "./CheckBox_logic";
 import styles from "./CheckBox.module.css";
+import {usePrefStore} from '@/app/global/store';
+import { useRouter } from "next/navigation";
+
 
 interface CheckboxProps {
   prefCode: number;
   prefName: string;
-  selectedPrefectures: number[];
 }
+
+interface PopulationData {
+  year: number;
+  value: number;
+  rate?: number;
+}
+
+interface PopulationCategory {
+  label: string;
+  data: PopulationData[];
+}
+
+//boundaryYearは2020でこれ以上は推定値
+interface PopulationResult {
+  prefName: string;
+  data: PopulationCategory[];
+}
+
+
 
 const Checkbox: React.FC<CheckboxProps> = ({
   prefCode,
   prefName,
-  selectedPrefectures,
 }) => {
+
   const [isChecked, setIsChecked] = useState(false);
 
+  //分割代入とselectorの書き方がある。再描画されるのは分割代入の方らしい
+  //https://reffect.co.jp/react/zustand#count-2
+
+  const removePrefecture = usePrefStore((state) => state.removePrefPopulationData);
+  //const increment = usePrefStore((state) => state.incrementCounter);
+
+  const router = useRouter();
+
   //具体的な処理を書く
-  const handleCheckboxChange = () => {
-    CheckBoxLogic(prefCode, !isChecked, selectedPrefectures);
+  const handleCheckboxChange = async () => {
+document.cookie = 'isAfterRemove=true';
+    console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbb");
+
+    if(isChecked){
+      //cookieの方でisAfterRemoveをtrueにする
+      
+      console.log("removePrefecture: " + prefName);
+      removePrefecture(prefName);
+      console.log("ここがどうなっている");
+      router.refresh();
+    }
+    else{
+      document.cookie = `prefCode=${encodeURIComponent(prefCode)}`;
+      document.cookie = `prefName=${encodeURIComponent(prefName)}`;
+      //cookieの方でisAfterRemoveをfalseにする
+      document.cookie = 'isAfterRemove=false';
+      //increment();
+      router.refresh();
+    }
+    
+
     setIsChecked(!isChecked);
+
   };
+
+  
 
   return (
 
-    <label className={`${styles.checkboxContainer} 
-    ${isChecked ? styles.checked : ""}`}>
-      <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={handleCheckboxChange}
-      />
+    <div>
+      <label className={`${styles.checkboxContainer} 
+        ${isChecked ? styles.checked : ""}`}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
       {prefName}
-    </label>
+      </label>
+
+    </div>
 
   );
 };
