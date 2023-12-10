e5a1863e99c31276fa9d735fb236539010bdfd93
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-11 05:57:05 +0900

    router関係のテストの手直し。

--- src/app/components/PrefecturesField/CheckBox/CheckBox.test.tsx
+++ src/app/components/PrefecturesField/CheckBox/CheckBox.test.tsx
@@ -1,70 +1,80 @@
 import "@testing-library/jest-dom";
 import { render, /*screen, */ fireEvent } from "@testing-library/react";
 import CheckBoxView from "./CheckBox_view";
-//import CheckBoxLogic from "./CheckBox_logic";
 import React from "react";
-//import userEvent from '@testing-library/user-event';
 
-//参考https://typescriptbook.jp/tutorials/component-test
+//参考
+//https://typescriptbook.jp/tutorials/component-test
 //https://stackoverflow.com/questions/69193525/how-to-get-the-correct-classname-in-react-testing-if-there-are-multiple-classnam
+
+/*
+
+*/
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      asPath: '/',
+      refresh: () => {return;},
+    };
+  },
+}));
+
+//jest.mock("next/navigation", () => ({ useRouter() { return { push: () => { return; }, }; },))
+//jest.mock('next/router', () => require('next-router-mock'));
+
+//jest.mock('next/navigation', () => require('next/router'));
+
 
 describe("Buttonコンポーネントのテスト", () => {
   //test1 チェックボックスのラベルが正しく表示されているかこれはTokyoと表示されるか
-  test("renders checkbox with correct label", () => {
+  test("test1 renders checkbox with correct label", () => {
     const { getByText } = render(
-      <CheckBoxView prefCode={1} prefName="青森" selectedPrefectures={[]} />,
+      <CheckBoxView prefCode={1} prefName="青森" cookieData="" />,
     );
 
     const labelElement = getByText(/青森/i);
     expect(labelElement).toBeInTheDocument();
   });
 
-  //test2 チェックボックスに配列が渡されたときにチェックボックスのprefCodeが正しく渡されるか
-  test("handles checkbox change", () => {
-    const selectedPrefectures: number[] = [];
+  //test2 チェックボックスにcookieDataが渡されたときにチェックボックスがチェックされているか
+  test("test2 handles checkbox change", () => {
     const { getByLabelText } = render(
       <CheckBoxView
         prefCode={2}
         prefName="青森"
-        selectedPrefectures={selectedPrefectures}
+        cookieData="青森"
       />,
     );
 
     const checkboxElement = getByLabelText(/青森/i);
+    expect(checkboxElement).toBeChecked();
 
-    // チェックボックスをクリックしてチェックを切り替える
-    fireEvent.click(checkboxElement);
-
-    // チェックが正しく切り替わったことを確認
-    expect(selectedPrefectures[0]).toEqual(2);
   });
 
+  //ここから下が成功しない
+  
   //test3 チェックボックスのスタイルが正しく適用されているか(デフォルト)
-  test("renders checkbox with default style", () => {
-    const { container } = render(
-      <CheckBoxView prefCode={1} prefName="青森" selectedPrefectures={[]} />,
+  test("test3 renders checkbox with default style", () => {
+    const { getByLabelText } = render(
+      <CheckBoxView prefCode={1} prefName="青森" cookieData="" />,
     );
-    expect(container.firstChild).toHaveClass("checkboxContainer");
+    const checkboxElement = getByLabelText(/青森/i);
+    expect(checkboxElement.parentNode).toHaveClass("checkboxContainer");
     // デフォルトスタイルが適用されていることを確認
-    expect(container.firstChild).not.toHaveClass("checked");
+    expect(checkboxElement.parentNode).not.toHaveClass("checked");
   });
 
   //test4 チェックボックスのスタイルが正しく適用されているか(チェックボックスクリック後)
-  test("changes style on checkbox click", () => {
-    const { container, getByLabelText } = render(
-      <CheckBoxView prefCode={1} prefName="青森" selectedPrefectures={[]} />,
+  test("test4 changes style on checkbox click", () => {
+    const { getByLabelText } = render(
+      <CheckBoxView prefCode={1} prefName="青森" cookieData="" />,
     );
 
-    const checkboxContainer = container.firstChild;
     const checkboxLabel = getByLabelText(/青森/i);
-
-    // チェックボックスがデフォルトスタイルであることを確認
-    expect(checkboxContainer).toHaveClass("checkboxContainer");
-    expect(checkboxContainer).not.toHaveClass("checked");
 
     // チェックボックスをクリックしてスタイルが変更されたことを確認
     fireEvent.click(checkboxLabel);
 
-    expect(checkboxContainer).toHaveClass("checked");
+    expect(checkboxLabel.parentNode).toHaveClass("checked");
   });
 });
