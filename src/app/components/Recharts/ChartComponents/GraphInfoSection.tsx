7aed73dd96282b5f81e539befb07313de368ae06
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-11 17:50:34 +0900

    タイトル、情報の出典、グラフ描画ツールとそれらに対するリンクの表示

---
+++ src/app/components/Recharts/ChartComponents/GraphInfoSection.tsx
@@ -0,0 +1,28 @@
+// GraphInfoSection.tsx
+import React from 'react';
+import styles from '../Recharts.module.css';
+
+interface GraphInfoSectionProps {
+  activeGraph: string;
+  boundaryYear: number;
+}
+
+const GraphInfoSection: React.FC<GraphInfoSectionProps> = ({ activeGraph, boundaryYear }) => (
+  <>
+    <div className={styles.title}>{activeGraph}推移</div>
+    <div className={styles.comment}>
+      ※{boundaryYear}年以降は推測値 出典:
+      <a href="https://resas.go.jp/" target="_blank" rel="noopener noreferrer" className={styles.externalLink}>
+        RESAS
+      </a>
+    </div>
+    <div className={styles.comment}>
+      ※グラフツール:
+      <a href="https://recharts.org/en-US/" target="_blank" rel="noopener noreferrer" className={styles.externalLink}>
+        Recharts
+      </a>
+    </div>
+  </>
+);
+
+export default GraphInfoSection;
