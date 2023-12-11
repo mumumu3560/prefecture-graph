4328bbad0e81c236506293e4ecbaf943f713195a
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-11 10:28:52 +0900

    自分で見ててわかりづらいのでコメントで説明をつける。

--- src/app/components/Recharts/recharts_data_converting.ts
+++ src/app/components/Recharts/recharts_data_converting.ts
@@ -19,19 +19,20 @@ interface PopulationData {
   type AllPrefecturesData = { year: number; [prefName: string]: number }[];
   
   type ResultType = {
-      総人口: AllPrefecturesData;
-      年少人口: AllPrefecturesData;
-      生産年齢人口: AllPrefecturesData;
-      老年人口: AllPrefecturesData;
+      "総人口": AllPrefecturesData;
+      "年少人口": AllPrefecturesData;
+      "生産年齢人口": AllPrefecturesData;
+      "老年人口": AllPrefecturesData;
       [key: string]: AllPrefecturesData; // インデックスシグネチャを追加
   };
   
+  //具体的な変換前後のデータはconvert_testで確認できる。
   export function convertPrefectureData(input: PopulationResult[]): ResultType {
       const result: ResultType = {
-          総人口: [],
-          年少人口: [],
-          生産年齢人口: [],
-          老年人口: []
+          "総人口": [],
+          "年少人口": [],
+          "生産年齢人口": [],
+          "老年人口": []
       };
   
       input.forEach(prefecture => {
@@ -42,11 +43,18 @@ interface PopulationData {
               const data = populationGroup.data;
   
               data.forEach(populationData => {
+                // AllPrefecturesData型の配列の中に、populationData.yearが含まれているものを探す。
+                //あれば{ year: 1960, '香川県': 1, '東京都': 9 }のようなデータを返す。
+                //なければundefinedを返す。
                   const existingData = result[label].find(d => d.year === populationData.year);
-  
+                
+                    // あれば、そのデータのprefNameの値をpopulationData.valueにする。
                   if (existingData) {
-                      existingData[prefName] = (existingData[prefName] || 0) + populationData.value;
+                      //c++でいうところのmapのようなものprefNameがkeyでpopulationData.valueがvalue
+                      existingData[prefName] = populationData.value;
+                      //existingData[prefName] = (existingData[prefName] || 0) + populationData.value;
                   } else {
+                    // 対応するyearがなければ新しいデータを作って、prefNameの値をpopulationData.valueにする。
                       const newData = { year: populationData.year, [prefName]: populationData.value };
                       result[label].push(newData);
                   }
