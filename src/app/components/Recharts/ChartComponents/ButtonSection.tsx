0e269041180212bae49ca20fd0eaff80eebf0ba6
mumumu3560 <puzmgamgam121@gmail.com>
2023-12-11 17:48:36 +0900

    チャートを切り替える部分

---
+++ src/app/components/Recharts/ChartComponents/ButtonSection.tsx
@@ -0,0 +1,18 @@
+// ButtonsSection.tsx
+import React from 'react';
+import styles from '../Recharts.module.css';
+
+interface ButtonsSectionProps {
+  handleGraphChange: (graph: string) => void;
+}
+
+const ButtonsSection: React.FC<ButtonsSectionProps> = ({ handleGraphChange }) => (
+  <div className={styles.buttons}>
+    <button onClick={() => handleGraphChange('総人口')}>総人口</button>
+    <button onClick={() => handleGraphChange('年少人口')}>年少人口</button>
+    <button onClick={() => handleGraphChange('生産年齢人口')}>生産年齢人口</button>
+    <button onClick={() => handleGraphChange('老年人口')}>老年人口</button>
+  </div>
+);
+
+export default ButtonsSection;
